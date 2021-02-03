2b32fd17d78942524c3a0f65f8f2c48da22c1483
Hannah <hannahwa@ualberta.ca>
2021-02-02 23:24:07 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const hitBackend = () => {
-    fetch('/')
-    .then((response) => {
-      console.log(response.data);
-    });
-  }
-  return (
-    <div className="App">
-      <button onClick={hitBackend}>Send request</button>
-    </div>
-  );
+  
 }
 
 export default App;
